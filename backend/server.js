20c89f57b8aa91f9d8e052b0747f7414a04ba23a
Kakashi1408 <perocasithole124@gmail.com>
2025-08-07 00:06:09 +0200

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,50 @@
+require("dotenv").config();
+const cors = require("cors");
+const express = require("express"); // importing express
+const mongoose = require("mongoose");
+// const listingsRoutes = require("./routes/listings");
+// const reservationRoutes = require("./routes/reservationRoute");
+// const userRoutes = require("./routes/user");
+
+// creating the express app
+const app = express();
+
+app.use(
+  cors({
+    origin: "", // Allow Netlify frontend
+    credentials: true,
+  })
+);
+
+//Middleware: Runs on every request. Like a bouncer checking names before letting people in
+app.use(express.json()); // This middleware works with a post request and checks if there is data or body to the request
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+//Route or Request handler: These will be unique per request. so each button press will have its own request handler.
+app.get("/", (req, res) => {
+  res.send("Backend is running!");
+});
+
+//use the imported listing routes
+
+// app.use("/api/listings", listingsRoutes);
+// app.use("/api/reservations", reservationRoutes);
+// app.use("/api/user", userRoutes);
+
+//connect to DB
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    //listen for requests and start the server on the port
+    const PORT = process.env.PORT;
+    app.listen(PORT, () => {
+      console.log("connected to db and listening on port", PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>
+          This is the Auth System Project
+        </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
